a469396ee83e90a64efbec4138258ffeb893f7f6
iltonkp <iltonk.si@gmail.com>
2019-11-24 14:55:53 -0300

    create main card

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   height: 100px;
   margin-top: 20px;
+  margin-bottom: 30px;
 `;
 export const TabsContainer = styled.ScrollView.attrs({
   horizontal: true,
